21e127443a09e55f96d245a94da3c81140b1133f
unknown <shankarkhade9@gmail.com>
2022-08-06 18:24:00 +0530

    this is file

---
+++ src/until/helper.js
@@ -0,0 +1,10 @@
+//let a  = function() {
+ //   return "Welcome to my application. I am shankar and a part of FunctionUp Plutonium cohort."
+//}
+
+//module.exports.name = batchName
+//module.exports.welcome = a
+const today=Date()
+console.log("today date ",today);
+console.log("plutonium,w3d5,topic for today is node js module system")
+module.exports.today=today
---
+++ src/validator/formatter.js
@@ -0,0 +1,5 @@
+const shiv="  functionUp  "
+console.log(shiv.trim());
+console.log(shiv.toUpperCase());
+console.log(shiv.toLowerCase());
+module.exports.shiv=shiv
