2946a1df3c9beac68e89b5bd255dbb7da966d6ef
Your Name <you@example.com>
2022-09-08 22:43:31 +0530

    ff

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,8 +2,8 @@
 const jwt = require("jsonwebtoken");
 const BlogsModel = require("../Models/BlogsModel");
 
-//---------------------------------------------AUTHENTICATION------------------------------//
-const authenticate = function (req, res, next) {
+
+const authenticate1 = function (req, res, next) {
 
     try {
         let token = req.headers["x-api-key"];
@@ -13,52 +13,71 @@ const authenticate = function (req, res, next) {
         let decodeToken = jwt.verify(token, "project1-secrete-key")
 
         if (!decodeToken) return res.status(403).send({ status: false, msg: "token is invalid" })
-
-        let authorToModify = req.query.authorId
-        let authorLoggedIn = decodeToken.authorId
-
-        if (authorToModify != authorLoggedIn) {
-            return res.status(403).send({ msg: " sorry! you are not a authorized person" })
-
-        }
 
         next()
     }
     catch (error) {
         res.status(500).send({ status: false, msg: error.message })
-    }
-};
-// ===========================================authorization=========================================//
+    }}
 
-const authorized = async function (req, res, next) {
-    try {
-        let token = req.headers["x-api-key"];
-        if (!token) return res.status(403).send({ status: false, msg: "token must be present" });
 
-        let decodeToken = jwt.verify(token, "project1-secrete-key")
+        //---------------------------------------------AUTHENTICATION------------------------------//
+        const authorizationByquery = function (req, res, next) {
 
-        if (!decodeToken) return res.status(403).send({ status: false, msg: "token is invalid" })
+            try {
+                let token = req.headers["x-api-key"];
 
-        let authorLoggedIn = decodeToken.authorId
+                if (!token) return res.status(403).send({ status: false, msg: "token must be present" });
 
-        let blogId = req.params.blogId
-        let data = await BlogsModel.findById({ _id: blogId })
-        if (data) {
-            if (data.authorId != authorLoggedIn) {
+                let decodeToken = jwt.verify(token, "project1-secrete-key")
 
-                return res.status(403).send({ msg: " sorry! you have not allowed" })
-            }
-            else {
+                if (!decodeToken) return res.status(403).send({ status: false, msg: "token is invalid" })
+
+                let authorToModify = req.query.authorId
+                let authorLoggedIn = decodeToken.authorId
+
+                if (authorToModify != authorLoggedIn) {
+                    return res.status(403).send({ msg: " sorry! you are not a authorized person" })
+
+                }
+
                 next()
             }
-        } else {
-            return res.status(403).send({ status: false, msg: "blogid is invalid" })
+            catch (error) {
+                res.status(500).send({ status: false, msg: error.message })
+            }
+        };
+        // ===========================================authorization=========================================//
+
+        const authorizationByid = async function (req, res, next) {
+            try {
+                let token = req.headers["x-api-key"];
+                if (!token) return res.status(403).send({ status: false, msg: "token must be present" });
+
+                let decodeToken = jwt.verify(token, "project1-secrete-key")
+
+                if (!decodeToken) return res.status(403).send({ status: false, msg: "token is invalid" })
+
+                let authorLoggedIn = decodeToken.authorId
+
+                let blogId = req.params.blogId
+                let data = await BlogsModel.findById({ _id: blogId })
+                if (data) {
+                    if (data.authorId != authorLoggedIn) {
+
+                        return res.status(403).send({ msg: " sorry! you have not allowed" })
+                    }
+                    else {
+                        next()
+                    }
+                } else {
+                    return res.status(403).send({ status: false, msg: "blogid is invalid" })
+                }
+            }
+
+            catch (error) {
+                res.status(500).send({ status: false, msg: error.message })
+            }
         }
-    }
 
-    catch (error) {
-        res.status(500).send({ status: false, msg: error.message })
-    }
-}
-
-module.exports = {authenticate,authorized}
+        module.exports = { authenticate1, authorizationByid, authorizationByquery }
--- src/routes/route.js
+++ src/routes/route.js
@@ -5,13 +5,13 @@ const AuthorController = require("../Controllers/AuthorController")
 const BlogController = require("../Controllers/BlogsController")
 const commonMid = require("../middleware/auth")
 
-
+//--------------------------------ti
 Router.post("/authors", AuthorController.createAuthor)
 Router.post("/login", AuthorController.loginAuthor)
-Router.post("/blogs",commonMid.authenticate, BlogController.createBlog)
-Router.get("/blogs",commonMid.authenticate, BlogController.getBlog)
-Router.put("/blogs/:blogId",commonMid.authorized, BlogController.updateBlog)
-Router.delete("/blogs/:blogId",commonMid.authorized, BlogController.deleteBlogs)
-Router.delete("/blogs",commonMid.authenticate, BlogController.deleteByquery)
+Router.post("/blogs",commonMid.authenticate1, BlogController.createBlog)
+Router.get("/blogs",commonMid.authorizationByquery, BlogController.getBlog)
+Router.put("/blogs/:blogId",commonMid.authenticate1,commonMid.authorizationByid, BlogController.updateBlog)
+Router.delete("/blogs/:blogId",commonMid.authorizationByid, BlogController.deleteBlogs)
+Router.delete("/blogs",commonMid.authorizationByquery, BlogController.deleteByquery)
 
 module.exports = Router
