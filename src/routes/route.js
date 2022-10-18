efa5f3f42005e076ab156ddb12b3988605f5ddd0
unknown <shankarkhade9@gmail.com>
2022-10-18 13:40:02 +0530

    done get api

--- src/routes/route.js
+++ src/routes/route.js
@@ -40,4 +40,67 @@ router.get('/student-details/:name', function(req, res){
     res.send('Dummy response')
 })
 
+router.get('/movies',function(req,res){
+    let movies=["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    res.send(movies)
+})
+
+router.get('/movies/:indexNumber',function(req,res){
+    let movies=["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+   let index = req.params.indexNumber
+   if(Number(index)>movies.lenght-1){
+    res.send('check ur indexno')
+   }else{
+   res.send(movies[index])}
+})
+
+router.get('/films',function(req,res){
+let film=[ {
+    id: 1,
+    name: "The Shining"
+   }, {
+    id: 2,
+    name: "Incendies"
+   }, {
+    id: 3,
+    name: "Rang de Basanti"
+   }, {
+    id: 4,
+    name: "Finding Nemo"
+   }]
+   const id=3
+   const films=film.find(film =>film.id==id)
+   if(film){
+    res.send(film)
+   }else{
+    res.send("no film and id exit")
+   }
+
+   
+})
+
+router.get('/films/:filmId',function(req,res){
+    let films=[ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+
+       let filmId = req.params.filmId
+       if(filmId>3){
+        res.send('film does not exit')
+       }else{
+       res.send(films[filmId])
+    console.log("its a good") }
+    })
+    
+
 module.exports = router;
